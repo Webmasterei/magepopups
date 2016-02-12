d9dfd616e9fe9d52a5f081478fcfa5f5a15d2de0
Bernhard Prange <info@webmasterei-prange.de>
2016-02-12 08:18:40 +0100

    js outsourcing, styles

--- app/code/community/Webmasterei/Magepopups/Helper/Data.php
+++ app/code/community/Webmasterei/Magepopups/Helper/Data.php
@@ -4,4 +4,8 @@
  */
 class Webmasterei_Magepopups_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    public function getHomeBlockId()
+    {
+        return Mage::getStoreConfig('magepopups/magepopups_blocks/magepopups_home',Mage::app()->getStore());
+    }
 }
--- js/magepopups/magepopups.js
+++ js/magepopups/magepopups.js
@@ -4,7 +4,7 @@ document.addEventListener( 'DOMContentLoaded', function () {
     var modal = document.getElementById('shadowbox');
 
     // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+    //var btn = document.getElementById("myBtn");
 
     // Get the <span> element that closes the modal
     var span = document.getElementById("close");
@@ -13,14 +13,14 @@ document.addEventListener( 'DOMContentLoaded', function () {
 
 
     // When the user clicks the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+   // btn.onclick = function() {
+       // modal.style.display = "block";
+    //}
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+   // span.onclick = function() {
+   //     modal.style.display = "none";
+   // }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
@@ -59,6 +59,87 @@ jQuery(document).ready(function ($) {
         window.getSelection().removeAllRanges();
     });
     }
+
+// Get the trigger
+    var trigger = $('#shadowbox').attr('trigger')
+    var exitintent_delay = $('#shadowbox').attr('exitintent_delay')
+    var scrolldepth_value =parseInt( $('#shadowbox').attr('scrolldepth'))
+    var inactivity_delay = $('#shadowbox').attr('inactivity_delay')
+    var timeonpage = $('#shadowbox').attr('timeonpage')
+    var cookie = Cookies.get('popup')
+    var opened = 0;
+///DEBUG VALUES
+//    console.log(trigger);
+//    console.log(exitintent_delay);
+//    console.log(scrolldepth_value);
+//    console.log(inactivity_delay);
+//    console.log(timeonpage);
+//    console.log(cookie);
+//    console.log(opened);
+// EXITINTENT
+    if(!cookie && trigger=="exitintent") {
+        $( document ).on( "mousemove", function( event ) {
+            if(event.pageY < 10 && opened==0) {
+                setTimeout(function() {
+                    $('#shadowbox').fadeIn();
+                },exitintent_delay);
+                opened = 1;
+            }
+        });
+    }
+// SCROLLDEPTH
+    if(!cookie && trigger=="scrolldepth") {
+        var height = $(document).height()-$(window).height();
+        var scrolldepth = 0;
+        var toTop = 0;
+        var cookie = Cookies.get('popup')
+        $(window).scroll(function(event) {
+            toTop = $(window).scrollTop();
+            scrolldepth = Math.round((toTop/height)*100);
+            if(scrolldepth >= scrolldepth_value && opened==0) {
+                if(!cookie) {
+                    $('#shadowbox').fadeIn();
+                    opened = 1
+                }
+            }
+        });
+    }
+// Inactivity
+    if(!cookie && trigger=="inactivity") {
+        idleTime = 0;
+        var idleInterval = setInterval(timerIncrement, 1000);
+
+        function timerIncrement() {
+            idleTime++;
+            if (idleTime > inactivity_delay) {
+                if (!cookie && opened == 0) {
+                    $('#shadowbox').fadeIn();
+                    opened = 1;
+                }
+            }
+        }
+
+        $(this).mousemove(function (e) {
+            idleTime = 0;
+        });
+    }
+// Time on Page
+    if(!cookie && trigger=="timeonpage") {
+        function showbox() {
+            $('#shadowbox').fadeIn();
+        }
+
+        var timeout = timeonpage * 1000;
+
+        if (!cookie) {
+            setTimeout(showbox, timeout);
+        }
+    }
+// Close button
+    $('#close').click(function(){
+        $('#shadowbox').fadeOut();
+        Cookies.set('popup', 'closed' , {expires:7});
+    });
 });
 
 
