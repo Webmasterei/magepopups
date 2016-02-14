ffcdc451fa03a09908c991d23bc45b84f750c23f
Bernhard Prange <info@webmasterei-prange.de>
2016-02-14 14:14:02 +0100

    css colors from admin

--- js/magepopups/magepopups.js
+++ js/magepopups/magepopups.js
@@ -1,66 +1,33 @@
-document.addEventListener( 'DOMContentLoaded', function () {
-
-    // Get the modal
-    var modal = document.getElementById('shadowbox');
-
-    // Get the button that opens the modal
-    //var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementById("close");
-
-
-
-
-    // When the user clicks the button, open the modal
-   // btn.onclick = function() {
-       // modal.style.display = "block";
-    //}
-
-    // When the user clicks on <span> (x), close the modal
-   // span.onclick = function() {
-   //     modal.style.display = "none";
-   // }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-}, false );
-
 jQuery(document).ready(function ($) {
 // Set cookie to not call Popup after subscription
     $('.newsletter button').click(function () {
         Cookies.set('popup','submitted');
     })
+    var modal = document.getElementById('shadowbox');
 
+    $(window).click( function(event) {
+        if (event.target ==  modal) {
+            $(modal).fadeOut();
+        }
+    });
     var copyEmailBtn = document.querySelector('.emailcopybtn');
     if(copyEmailBtn) {
     copyEmailBtn.addEventListener('click', function(event) {
-        // Select the email link anchor text
         var emailLink = document.querySelector('.coupon input');
         var range = document.createRange();
         range.selectNode(emailLink);
         window.getSelection().addRange(range);
-
         try {
-            // Now that we've selected the anchor text, execute the copy command
             var successful = document.execCommand('copy');
             var msg = successful ? 'successful' : 'unsuccessful';
             $('.coupon input').css('background-color',"#2c87f0");
             $('.coupon input').css('color',"white");
         } catch(err) {
         }
-
-        // Remove the selections - NOTE: Should use
-        // removeRange(range) when it is supported
         window.getSelection().removeAllRanges();
     });
     }
 
-// Get the trigger
     var trigger = $('#shadowbox').attr('trigger')
     var exitintent_delay = $('#shadowbox').attr('exitintent_delay')
     var scrolldepth_value =parseInt( $('#shadowbox').attr('scrolldepth'))
@@ -68,26 +35,18 @@ jQuery(document).ready(function ($) {
     var timeonpage = $('#shadowbox').attr('timeonpage')
     var cookie = Cookies.get('popup')
     var opened = 0;
-///DEBUG VALUES
-//    console.log(trigger);
-//    console.log(exitintent_delay);
-//    console.log(scrolldepth_value);
-//    console.log(inactivity_delay);
-//    console.log(timeonpage);
-//    console.log(cookie);
-//    console.log(opened);
-// EXITINTENT
+
     if(!cookie && trigger=="exitintent") {
-        $( document ).on( "mousemove", function( event ) {
-            if(event.pageY < 10 && opened==0) {
+        $(document).on('mouseleave', leaveFromTop);
+        function leaveFromTop(e){
+            if( e.clientY < 60 && opened==0) {
                 setTimeout(function() {
                     $('#shadowbox').fadeIn();
                 },exitintent_delay);
-                opened = 1;
+            opened = 1;
             }
-        });
+        }
     }
-// SCROLLDEPTH
     if(!cookie && trigger=="scrolldepth") {
         var height = $(document).height()-$(window).height();
         var scrolldepth = 0;
@@ -104,7 +63,6 @@ jQuery(document).ready(function ($) {
             }
         });
     }
-// Inactivity
     if(!cookie && trigger=="inactivity") {
         idleTime = 0;
         var idleInterval = setInterval(timerIncrement, 1000);
@@ -123,22 +81,21 @@ jQuery(document).ready(function ($) {
             idleTime = 0;
         });
     }
-// Time on Page
     if(!cookie && trigger=="timeonpage") {
         function showbox() {
             $('#shadowbox').fadeIn();
         }
-
         var timeout = timeonpage * 1000;
-
         if (!cookie) {
             setTimeout(showbox, timeout);
         }
     }
-// Close button
     $('#close').click(function(){
         $('#shadowbox').fadeOut();
         Cookies.set('popup', 'closed' , {expires:7});
+    });
+    $('#deletecookie').click( function() {
+            Cookies.remove('popup');
     });
 });
 
